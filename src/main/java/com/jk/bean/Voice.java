988448eba25314abc5e3ceda9338d60f8dadffc6
wj <wj332100@163.com>
2019-01-26 21:01:02 +0800

    更新

---
+++ src/main/java/com/jk/bean/Voice.java
@@ -0,0 +1,23 @@
+package com.jk.bean;
+
+import lombok.Data;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author 王杰
+ * @create 2019/1/26
+ * @since 1.0.0
+ */
+@Data
+public class Voice {
+
+    private  String code;
+
+    private  String data;
+
+    private  String desc;
+
+    private  String sid;
+}
